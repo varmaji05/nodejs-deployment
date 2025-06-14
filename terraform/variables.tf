1293fc329f39c18eab49d727fd912a86886b9aa8
varmaji05 <varmavinod101@gmail.com>
2025-06-14 11:21:03 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,33 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "project" {
+  default = "demo"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  type = map(object({
+    cidr = string
+    az   = string
+  }))
+  default = {
+    "pub-1" = { cidr = "10.0.1.0/24", az = "us-east-1a" },
+    "pub-2" = { cidr = "10.0.2.0/24", az = "us-east-1b" }
+  }
+}
+
+variable "private_subnets" {
+  type = map(object({
+    cidr = string
+    az   = string
+  }))
+  default = {
+    "priv-1" = { cidr = "10.0.11.0/24", az = "us-east-1a" },
+    "priv-2" = { cidr = "10.0.12.0/24", az = "us-east-1b" }
+  }
+}
