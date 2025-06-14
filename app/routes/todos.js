e69306af8cee7e5b5818e6c39e2fc54e0770c64b
varmaji05 <varmavinod101@gmail.com>
2025-06-14 10:07:20 +0530

    Update todos.js

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -1,19 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const todos = [
-  { id: 1, task: "Learn Docker", completed: false },
-  { id: 2, task: "Write Terraform", completed: false }
-];
+const todoController = require('../controllers/todoController');
 
-router.get('/', (req, res) => {
-  res.json(todos);
-});
-
-router.post('/', (req, res) => {
-  const newTodo = req.body;
-  todos.push(newTodo);
-  res.status(201).json(newTodo);
-});
+router.get('/', todoController.getTodos);
+router.post('/', todoController.createTodo);
 
 module.exports = router;
