9e67ac3fdd518dcd3aabf097a5c4ff98d366f755
varmaji05 <varmavinod101@gmail.com>
2025-06-14 11:20:07 +0530

    Create route_table.tf

---
+++ terraform/route_table.tf
@@ -0,0 +1,19 @@
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project}-public-rt"
+  }
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public" {
+  for_each = aws_subnet.public
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
