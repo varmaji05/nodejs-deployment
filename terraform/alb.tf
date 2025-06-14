e2d09717a2b8e171eccdfdf137754fc0428202d1
varmaji05 <varmavinod101@gmail.com>
2025-06-14 11:25:51 +0530

    Create alb.tf

---
+++ terraform/alb.tf
@@ -0,0 +1,38 @@
+resource "aws_lb" "app_alb" {
+  name               = "${var.project}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = var.public_subnet_ids
+
+  tags = {
+    Name = "${var.project}-alb"
+  }
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "${var.project}-tg"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/health"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
