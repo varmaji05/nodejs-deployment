5d5cec97a29458167fd73ec089f5ca93ccc3db21
varmaji05 <varmavinod101@gmail.com>
2025-06-14 12:16:12 +0530

    Update user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -3,3 +3,23 @@ apt update -y
 apt install -y docker.io
 usermod -aG docker ubuntu
 systemctl enable docker
+
+apt install -y docker.io docker-compose git -y
+systemctl enable docker
+systemctl start docker
+
+# Clone your GitHub repo (replace with your actual repo URL)
+cd /home/ubuntu
+git clone https://github.com/varmaji05/nodejs-deployment.git
+cd app
+
+# Create the .env file with dynamic values passed from Terraform
+cat <<EOF > .env
+PORT=3000
+DB_HOST=${db_host}
+DB_USER=${db_user}
+DB_PASS=${db_pass}
+EOF
+
+# Start Docker container
+docker-compose up -d
