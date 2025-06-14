de1aea7a4faa15960018f87bd5f196a22210a249
varmaji05 <varmavinod101@gmail.com>
2025-06-14 11:26:13 +0530

    Create security.tf

---
+++ terraform/security.tf
@@ -0,0 +1,39 @@
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.project}-alb-sg"
+  description = "Allow HTTP"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "${var.project}-ec2-sg"
+  description = "Allow app port"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port       = 3000
+    to_port         = 3000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
