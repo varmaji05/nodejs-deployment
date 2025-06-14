4ec68936429f8ddb3a7e931dfd454419e2c86b82
varmaji05 <varmavinod101@gmail.com>
2025-06-14 11:25:05 +0530

    Create ec2.tf

---
+++ terraform/ec2.tf
@@ -0,0 +1,43 @@
+resource "aws_launch_template" "app_lt" {
+  name_prefix   = "${var.project}-lt"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.ec2_sg.id]
+  }
+
+  user_data = base64encode(templatefile("${path.module}/user_data.sh", {
+    ecr_repo_url = aws_ecr_repository.app.repository_url
+  }))
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "app_asg" {
+  name                      = "${var.project}-asg"
+  min_size                  = 2
+  max_size                  = 4
+  desired_capacity          = 2
+  vpc_zone_identifier       = var.public_subnet_ids
+  target_group_arns         = [aws_lb_target_group.app_tg.arn]
+  launch_template {
+    id      = aws_launch_template.app_lt.id
+    version = "$Latest"
+  }
+
+  health_check_type         = "EC2"
+  health_check_grace_period = 300
+
+  tag {
+    key                 = "Name"
+    value               = "${var.project}-instance"
+    propagate_at_launch = true
+  }
+
+  depends_on = [aws_lb_listener.http]
+}
