0e8e092be80d42737d0a8f3b32c804a99c449329
varmaji05 <varmavinod101@gmail.com>
2025-06-14 11:48:09 +0530

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -36,3 +36,17 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.app_tg.arn
   }
 }
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.ssl_cert.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
