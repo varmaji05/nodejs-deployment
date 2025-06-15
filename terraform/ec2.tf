f253f343d2e3c84a08e1c8ca61d33f32dae23aed
varmaji05 <varmavinod101@gmail.com>
2025-06-15 21:07:13 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,8 +3,9 @@ resource "aws_launch_template" "app_lt" {
   image_id      = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
-  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
-
+  iam_instance_profile = { 
+    name = aws_iam_instance_profile.ec2_instance_profile.name
+  }
   network_interfaces {
     associate_public_ip_address = true
     security_groups             = [aws_security_group.ec2_sg.id]
