5b697085007ac8dd4511bdea209098357bffe162
varmaji05 <varmavinod101@gmail.com>
2025-06-15 20:40:28 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,13 @@
-variable "aws_region" {
+variable "region" {
   default = "us-east-1"
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
 }
 
 variable "project" {
