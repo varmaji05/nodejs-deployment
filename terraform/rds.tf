b275077620b6b81891d6bece3c6a64f99d6af24d
varmaji05 <varmavinod101@gmail.com>
2025-06-15 20:22:43 +0530

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -12,8 +12,8 @@ resource "aws_db_instance" "postgres" {
   instance_class          = "db.t3.micro"
   allocated_storage       = 20
   storage_type            = "gp2"
-  username                = var.db_username
-  password                = var.db_password
+  username                = jsondecode(aws_secretsmanager_secret_version.db_creds_version.secret_string)["username"]
+  password                = jsondecode(aws_secretsmanager_secret_version.db_creds_version.secret_string)["password"]
   db_subnet_group_name    = aws_db_subnet_group.db_subnet_group.name
   vpc_security_group_ids  = [aws_security_group.db_sg.id]
   publicly_accessible     = false
