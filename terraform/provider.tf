a0d271f1cdfdd7301d13087993b59fd5d87fc6ea
varmaji05 <varmavinod101@gmail.com>
2025-06-15 20:40:31 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region = var.region
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
