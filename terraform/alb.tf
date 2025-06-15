188a32324abdfcf905401e4e99af2f9eb8e67e11
varmaji05 <varmavinod101@gmail.com>
2025-06-15 20:53:08 +0530

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "app_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = var.public_subnet_ids
+  subnets            = values(aws_subnet.public)[*].id
 
   tags = {
     Name = "${var.project}-alb"
