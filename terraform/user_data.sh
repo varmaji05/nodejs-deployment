8707e22f4fe6f63710bfbd6f485ce0ef7327efc0
varmaji05 <varmavinod101@gmail.com>
2025-06-16 19:20:00 +0530

    Update user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Update and install Docker on Ubuntu
+# Update and install Docker
 apt-get update -y
 apt-get install -y docker.io
 
@@ -7,9 +7,12 @@ apt-get install -y docker.io
 systemctl start docker
 systemctl enable docker
 
-# Login to ECR
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ecr_repo_url}
+# Install AWS CLI if needed (optional if AMI already includes it)
+apt-get install -y awscli
 
-# Pull and run the Docker image
+# Login to Amazon ECR
+aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${ecr_repo_url}
+
+# Pull and run Docker container WITHOUT .env file
 docker pull ${ecr_repo_url}:latest
-docker run -d --env-file /app/.env -p 3000:3000 --name nodejs-api ${ecr_repo_url}:latest
+docker run -d -p 3000:3000 --name nodejs-api ${ecr_repo_url}:latest
