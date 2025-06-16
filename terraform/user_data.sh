fc788afebc6ea89d46451eab57a8075800693f61
varmaji05 <varmavinod101@gmail.com>
2025-06-16 19:25:59 +0530

    Update user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -15,4 +15,10 @@ aws ecr get-login-password --region ${region} | docker login --username AWS --pa
 
 # Pull and run Docker container WITHOUT .env file
 docker pull ${ecr_repo_url}:latest
-docker run -d -p 3000:3000 --name nodejs-api ${ecr_repo_url}:latest
+docker run -d \
+  -e DB_HOST=${db_host} \
+  -e DB_USER=${db_user} \
+  -e DB_PASS=${db_pass} \
+  -p 3000:3000 \
+  --name nodejs-api \
+  ${ecr_repo_url}:latest
